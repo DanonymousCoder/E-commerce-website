d76e63a726802da38b4ba28b2f9d0fefe9f0d208
Ridwan <aladebaruwa@gmail.com>
2024-07-24 09:49:39 +0100

    Added mobile menu functionality and styles

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ function copyMenu() {
     var topNav = document.querySelector('.header-top .wrapper');
     var topPlace = document.querySelector(".off-canvas .thetop-nav");
     topPlace.innerHTML = topNav.innerHTML;
-
 }
 copyMenu();
+
+// show mobile menu
+const menuButton = document.querySelector('.trigger'),
+        closeButton = document.querySelector('.t-close'),
+        addClass = document.querySelector('.site');
+menuButton.addEventListener('click', function() {
+    addClass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function () {
+    addClass.classList.remove('showmenu')
+})
+
+
+
+
+// show sub menu on mobile
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
+}
